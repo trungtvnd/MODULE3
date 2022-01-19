18e3fb4568814c24bca3f66560f61a947013759c
trungtvnd <tranviettrung20011992@gmail.com>
2022-01-19 19:03:26 +0700

    add file

---
+++ TUAN 12/BUOI 58/thuchanhprocedure.sql
@@ -0,0 +1,25 @@
+SELECT * FROM customers WHERE customerName = 'Land of Toys Inc.'; 
+EXPLAIN SELECT * FROM customers WHERE customerName = 'Land of Toys Inc.'; 
+
+ALTER TABLE customers ADD INDEX idx_customerName(customerName);
+EXPLAIN SELECT * FROM customers WHERE customerName = 'Land of Toys Inc.'; 
+
+
+ALTER TABLE customers ADD INDEX idx_full_name(contactFirstName, contactLastName);
+EXPLAIN SELECT * FROM customers WHERE contactFirstName = 'Jean' or contactFirstName = 'King';
+SELECT * FROM customers WHERE contactFirstName = 'Jean' or contactFirstName = 'King';
+
+DELIMITER //
+
+CREATE PROCEDURE findAllCustomers()
+
+BEGIN
+
+  SELECT * FROM customers;
+
+END //
+
+DELIMITER ;
+call findAllCustomers();
+DELIMITER //
+DROP PROCEDURE IF EXISTS `findAllCustomers`//
