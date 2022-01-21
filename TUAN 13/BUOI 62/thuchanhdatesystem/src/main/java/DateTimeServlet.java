17545890f60e48bbf596b19b47dc4b339a3f92e0
trungtvnd <tranviettrung20011992@gmail.com>
2022-01-21 21:27:27 +0700

    add file

---
+++ TUAN 13/BUOI 62/thuchanhdatesystem/src/main/java/DateTimeServlet.java
@@ -0,0 +1,26 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+@WebServlet(name = "DateTimeServlet", value = "/DateTimeServlet")
+public class DateTimeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Hello world</h1>");
+        writer.println("</html>");
+        Date today = new Date();
+
+        writer.println("<h1>" + today + "</h1>");
+        writer.println("</html>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
